519d2624054d4f84042199d059312fae2ceb48eb
rkhexed <64305387+rkhexed@users.noreply.github.com>
2025-01-06 09:06:20 -0500

    Created a function to get all pdf names (needs testing)

--- pdf_parse.py
+++ pdf_parse.py
@@ -1,9 +1,22 @@
 import pdfplumber
 import PyPDF2
-
+import os
 from PyPDF2 import PdfReader
 
+
+def GetFolderContents(path):
+    dirListing = os.listdir(path)
+    editFiles = []
+    for item in dirListing:
+        if ".pdf" in item:
+            editFiles.append(path+'\\'+item)
+    print(editFiles)
+
+GetFolderContents("C:\Users\Kaushal\Desktop\RAG\PDF_collection")
+
+"""
 reader = PdfReader("")
 number_of_pages = len(reader.pages)
 page = reader.pages[0]
 text = page.extract_text()
+"""
